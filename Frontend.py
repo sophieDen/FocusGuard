007bd906ebc51ee75aca81794fbf1bf9f093abc3
Sophie Denysiuk <88908991+sophieDen@users.noreply.github.com>
2026-03-17 02:08:40 +0000

    merged and tested the eye gaze

--- Frontend.py
+++ Frontend.py
@@ -25,8 +25,6 @@ from core.base_detector import DetectionResult
 from modules.lighting.lighting_detector import LightingDetector
 from modules.gaze.gaze_detector import GazeDetector
 
-# from modules.gaze.gaze_detector import GazeDetector
-# from modules.posture.posture_detector import PostureDetector
 import config
 
 
